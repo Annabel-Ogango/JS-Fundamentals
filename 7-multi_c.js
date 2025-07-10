171c84f9dbe19e095deee251fa2271d27ff8e1b2
Annabel <annabelogango@gmail.com>
2025-07-11 01:46:33 +0300

    Update 7-multi_c.js to use while loop

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -3,7 +3,9 @@ const count = parseInt(process.argv[2]);
 if(isNaN(count)) {
     console.log('Missing number of occurrences');
 } else {
-    for(let i=0; i<count; i++) {
-        console.log('C is fun');
+    let i=0;
+    while (i < count) {
+        console.log ('C is fun');
+        i++;
     }
 }
